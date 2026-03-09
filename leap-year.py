bbfba0fcf2495619ec514a5f30d21ca93f2ba516
adhamcodes <adhammahmood83@gmail.com>
2026-03-09 23:22:26 +0600

    Fix leap year check logic and improve input handling

--- leap-year.py
+++ leap-year.py
@@ -14,3 +14,13 @@ if year % 4 == 0:
         print(year, "is a leap year.")
 else:
     print(year, "is not a leap year.")
+
+
+#Method 2
+
+year = input("Enter the year: ")
+year = int(year)
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
