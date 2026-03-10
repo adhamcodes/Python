a3f29f39a4beddc026d5d1b23a5725929a9edb4b
adhamcodes <adhammahmood83@gmail.com>
2026-03-10 23:24:06 +0600

    Add program to find the largest number out of two user-input numbers

---
+++ Q14.py
@@ -0,0 +1,9 @@
+"""Write a program to find the largest number out of two numbers excepted from user."""
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num1 > num2:
+    print(f"The largest number is: {num1}")
+elif num2 > num1:
+    print(f"The largest number is: {num2}")
+else:
+    print("Both numbers are equal.")
---
+++ Q15.py
@@ -0,0 +1,8 @@
+"""Write a program to check whether a number (accepted from user) is positive or negative."""
+number = float(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
