452f2c0ef33e815bcef09657a1f2d6eed0be0ac1
adhamcodes <adhammahmood83@gmail.com>
2026-03-10 23:22:18 +0600

    Add program to check if a person is a senior citizen based on age input

---
+++ Q12.py
@@ -0,0 +1,6 @@
+"""Write a program to check whether a person is senior citizen or not."""
+age = int(input("Enter the age of the person: "))
+if age >= 60:
+    print("The person is a senior citizen.")
+else:
+    print("The person is not a senior citizen.")
